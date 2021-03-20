8bb2bdb9fb0022169e479444d034af4942f72fdb
xanun <paotola@outlook.com>
2021-03-20 23:09:33 +0700

    Initial commit

---
+++ src/com/xanun/Main.java
@@ -0,0 +1,18 @@
+package com.xanun;
+
+public class Main {
+static class Human {
+    String Name;
+    int Age;
+    String Company;
+}
+    public static void main(String[] args) {
+	// write your code here
+    Human tolapao = new Human();
+    tolapao.Name = "tolapao";
+    tolapao.Age = 23;
+        System.out.println(tolapao.Name);
+        System.out.println(tolapao.Age);
+
+    }
+}
